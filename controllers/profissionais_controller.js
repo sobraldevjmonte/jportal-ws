3884c0fb63c78895567366d04761343c202e37de
ednaldosobraldev <104601650+ednaldosobraldev@users.noreply.github.com>
2024-07-23 15:49:07 -0300

    ajuste consulta vendas

--- controllers/profissionais_controller.js
+++ controllers/profissionais_controller.js
@@ -55,17 +55,49 @@ exports.listarUsuarios = async (req, res) => {
   }
 };
 exports.listarPedidos = async (req, res) => {
-  let sqlPedidos = "SELECT * FROM vendas";
+  const pathImagem = "/anexos";
+  let sqlPedidos =
+    "SELECT " +
+    "   v.id_vendas, " +
+    "   v.numero_venda, " +
+    "   v.numero_np, " +
+    "   TO_CHAR(v.data_venda,'DD/MM/YYYY') as data_venda, " +
+    "   TO_CHAR(v.data_lancamento,'DD/MM/YYYY') as data_lancamento, " +
+    "   l.descricao_loja, " +
+    "   TO_CHAR(v.data_np,'DD/MM/YYYY') as data_np, " +
+    "   TO_CHAR(v.data_pagamento, 'DD/MM/YYYY') as data_pagamento," +
+    "   v.valor as valor_np, " +
+    "   u.nome as profissional, " +
+    "   v.status, " +
+    "   v.total_pontos, " +
+    "   v.imagem, " +
+    "   v.rejeicoes, " +
+    "   v.motivo_rejeicao, " +
+    "   v.premiado " +
+    "FROM " +
+    "   vendas v " +
+    "   left join usuarios u on " +
+    "     v.id_usuario = u.id_usuario " +
+    "   left join lojas l on " +
+    "     v.id_loja = l.id_loja_venda";
   console.log("************** listarPedidos ********************");
+  console.log(sqlPedidos)
 
   try {
     let rs = await pg.execute(sqlPedidos);
     let countPedidos = rs.rows.length;
 
+    // Adicionar o caminho completo da imagem a cada registro
+    const listaPedidosComImagem = rs.rows.map((pedido) => ({
+      ...pedido,
+      imagem: `${pathImagem}/${pedido.imagem}`, // Ajuste este campo conforme o nome real da coluna
+    }));
+
     const response = {
       registros: countPedidos,
       //totais_np: rst.rows,
-      lista_pedidos: rs.rows,
+      lista_pedidos: listaPedidosComImagem,
+      // lista_pedidos: rs.rows,
     };
     res.status(200).send(response);
   } catch (error) {
