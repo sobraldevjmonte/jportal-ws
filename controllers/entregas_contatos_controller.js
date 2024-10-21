993a53135ee7d98134052ebf352efbfe920e2b3f
ednaldosobraldev <104601650+ednaldosobraldev@users.noreply.github.com>
2024-10-21 16:36:26 -0300

    ajuste tela gerente lista contatos entregas

--- controllers/entregas_contatos_controller.js
+++ controllers/entregas_contatos_controller.js
@@ -60,15 +60,16 @@ exports.buscarDadosEntregas = async (req, res) => {
         fone,
         celular
       FROM vs_pwb_fprevendas x 
-      WHERE x.data_compromisso >= CURRENT_DATE - 30 and cod_loja_pre is not null`);
-    
+      WHERE data_compromisso >= CURRENT_DATE - INTERVAL '5 days' AND data_compromisso < CURRENT_DATE - INTERVAL '1 day'
+  AND cod_loja_pre IS NOT NULL`);
+
     const rows = resProd.rows;
-    console.log('tamanho: ' + rows.length);
+    console.log("tamanho: " + rows.length);
 
     // 2. Inserir dados no banco Y
     if (rows.length > 0) {
       console.log("************* inserindo *******************");
-      
+
       // Obtenha os nomes das colunas da tabela de destino
       const columnNames = `
         cod_loja_pre, np, data_pre, data_fat, data_compromisso, 
@@ -89,7 +90,7 @@ exports.buscarDadosEntregas = async (req, res) => {
         const npExistsQuery = `
           SELECT COUNT(*) FROM entregas_contatos WHERE np = '${row.np}' and cod_loja_pre = '${row.cod_loja_pre}'`;
         const existsRes = await pg.execute(npExistsQuery);
-        
+
         if (existsRes.rows[0].count > 0) {
           console.log(`NP ${row.np} já existe. Ignorando inserção.`);
           continue; // Pular a inserção se a np já existir
@@ -98,24 +99,26 @@ exports.buscarDadosEntregas = async (req, res) => {
         // Preparar a consulta de inserção
         const insertQuery = `
           INSERT INTO entregas_contatos (${columnNames}) VALUES (
-            ${Object.values(row).map((value, index) => {
-              // Verifica se o valor é nulo e trata de acordo
-              if (value === null) return "NULL";
-              
-              // Se o valor é uma data, converta usando dayjs
-              if (index === 2 || index === 3 || index === 4 || index === 28) { // data_pre, data_fat, data_compromisso, data_proximo
-                return `'${dayjs(value).format("YYYY-MM-DD")}'`; // Formato adequado para o PostgreSQL
-              }
-              
-              return `'${value}'`;
-            }).join(", ")}
+            ${Object.values(row)
+              .map((value, index) => {
+                // Verifica se o valor é nulo e trata de acordo
+                if (value === null) return "NULL";
+
+                // Se o valor é uma data, converta usando dayjs
+                if (index === 2 || index === 3 || index === 4 || index === 28) {
+                  // data_pre, data_fat, data_compromisso, data_proximo
+                  return `'${dayjs(value).format("YYYY-MM-DD")}'`; // Formato adequado para o PostgreSQL
+                }
+
+                return `'${value}'`;
+              })
+              .join(", ")}
           );
         `;
-        
 
         console.log(insertQuery);
         // Executar a inserção
-        console.log('sequencia: ' + xxx)
+        console.log("sequencia: " + xxx);
         await pg.execute(insertQuery);
         xxx++;
       }
@@ -167,11 +170,12 @@ exports.alterarObsVendedor = async (req, res) => {
   };
   res.status(201).send(response);
 };
+
 exports.listarEntregasContatosVendedor = async (req, res) => {
   console.log("******** listarEntregasContatosVendedor *********");
-  
+
   let idVendedor = req.params.idVendedor;
-  console.log(idVendedor)
+  console.log(idVendedor);
   let sqlEntregasContatos =
     "SELECT " +
     "   MAX(ec.id) AS id, " +
@@ -211,13 +215,113 @@ exports.listarEntregasContatosVendedor = async (req, res) => {
 
   //'LIMIT 5';
   console.log(sqlEntregasContatos);
- 
+
   try {
     let rs = await pg.execute(sqlEntregasContatos, [idVendedor]);
     let countEntregas = rs.rows.length;
 
     const response = {
       registros: countEntregas,
+      lista_contatos: rs.rows,
+    };
+    res.status(200).send(response);
+  } catch (error) {
+    console.log(error);
+    return res.status(404).send({ error: error, mensagem: "Erro ao procurar" });
+  }
+};
+
+exports.listaVendedoresDoGerente = async (req, res) => {
+  console.log("******** listarEntregasContatosGerente *********");
+
+  let idLoja = req.params.idLoja;
+  console.log('idLoja: ' + idLoja);
+  
+  let sqlListaVendedoresDoGerente =
+    "select " +
+    "   distinct(ec.cod_vendedor_pre) as codVendedor, " +
+    "   ec.cod_loja_pre , " +
+    "   ec.vendedor " +
+    "from " +
+    "   entregas_contatos ec " +
+    "where " +
+    "	  ec.cod_loja_pre = $1 " +
+    "and " + 
+    "    ec.status = 'Finalizado' " +
+    "and " + 
+    "    ec.tipoentrega = 'LojaEntrega' " +
+    "and " + 
+    "    ec.data_compromisso <= CURRENT_DATE - interval '1 day' " +
+    "order by ec.vendedor ";
+  console.log(sqlListaVendedoresDoGerente);
+
+  try {
+    let rs = await pg.execute(sqlListaVendedoresDoGerente, [idLoja]);
+    let countVendedoresDoGerente = rs.rows.length;
+
+    const response = {
+      registros: countVendedoresDoGerente,
+      lista_contatos: rs.rows,
+    };
+    res.status(200).send(response);
+  } catch (error) {
+    console.log(error);
+    return res.status(404).send({ error: error, mensagem: "Erro ao procurar" });
+  }
+};
+
+
+exports.listarEntregasContatosGerente = async (req, res) => {
+  console.log("******** listarEntregasContatosGerente *********");
+
+  let idLoja = req.params.idLoja;
+  console.log(idLoja);
+  let sqlEntregasContatosGerente =
+    "SELECT " +
+    "   MAX(ec.id) AS id, " +
+    "   MAX(ecd.id) AS idDetalhe, " +
+    "   ec.cod_loja_pre as codigoLoja, " +
+    "   REPLACE(ec.np, ' ', '') AS np, " +
+    "   TO_CHAR(ec.data_compromisso, 'DD/MM/YYYY') as dataCompromisso, " +
+    "   TO_CHAR(ec.data_pre, 'DD/MM/YYYY') as dataPreVenda, " +
+    "   ec.cod_cliente_pre as codigoCliente, " +
+    "   ec.cod_vendedor_pre as codVendedor, " +
+    "   ec.status, " +
+    "   SUM(ec.vlr_total) AS valorTotal, " +
+    "   MAX(ecd.obs) AS obs, " +
+    "   ec.cliente, " +
+    "   ec.vendedor, " +
+    "   ec.fone, " +
+    "   ec.celular, " +
+    "   ec.tipoentrega " +
+    "FROM " +
+    "   entregas_contatos ec " +
+    "LEFT JOIN entregas_contatos_detallhes ecd ON " +
+    "   REPLACE(ec.np, ' ', '') = ecd.np " +
+    "WHERE " +
+    "   ec.cod_loja_pre = $1 " +
+    "AND " +
+    "   ec.status = 'Finalizado' " +
+    "AND " +
+    "   ec.tipoentrega = 'LojaEntrega' " +
+    "AND " +
+    "   ec.data_compromisso < CURRENT_DATE - INTERVAL '1 day' " +
+    "GROUP BY " +
+    "   ec.cod_loja_pre, ec.np, ec.data_compromisso, ec.data_pre, " +
+    "   ec.cod_cliente_pre, ec.cod_vendedor_pre, ec.status, ec.cliente, " +
+    "   ec.vendedor, ec.fone, ec.celular, ec.tipoentrega " +
+    "ORDER BY " +
+    "   ec.data_compromisso DESC";
+
+  //'LIMIT 5';
+  console.log(sqlEntregasContatosGerente);
+
+  try {
+    let rs = await pg.execute(sqlEntregasContatosGerente, [idLoja]);
+    let countEntregasGerente = rs.rows.length;
+
+    const response = {
+      registros: countEntregasGerente,
       lista_contatos: rs.rows,
     };
     res.status(200).send(response);
--- routes/entregas_contatos_router.js
+++ routes/entregas_contatos_router.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const entregasContatosController = require("../controllers/entregas_contatos_controller");
 
 
+//******************** vendedores  **************/
 router.get("/entregas-contatos/lista-entregas-contatos-vendedor/:idVendedor", entregasContatosController.listarEntregasContatosVendedor);
 router.post("/entregas-contatos/salvar-obs-vendedor", entregasContatosController.salvarObsVendedor);
 router.put("/entregas-contatos/alterar-obs-vendedor", entregasContatosController.alterarObsVendedor);
+
+//******************** gerente  **************/
+router.get("/entregas-contatos/lista-vendedores-do-gerente/:idLoja", entregasContatosController.listaVendedoresDoGerente);
+router.get("/entregas-contatos/lista-entregas-contatos-gerente/:idVendedor", entregasContatosController.listarEntregasContatosGerente);
+
 
 module.exports = router;
